d92b970a22b5bd54a6d96dddd010d21022d1e615
natmartin89 <natalie.martin@litfibre.com>
2023-01-16 08:00:30 +0000

    Added form
    Added JS to "surprise me" button and added form element to collect user information.

---
+++ resources/index.js
@@ -0,0 +1,8 @@
+function randomCity() {
+  alert("Let's go to London!");
+}
+
+let element = document.querySelector("#surprisebtn");
+element.addEventListener("click", randomCity());
+
+let name = document.getElementById("Name");
